ae987639b80abc0a4e712b5ae42cd92a29d1546a
Mingxuan Liu <2019011798lmx@gmail.com>
2026-01-23 11:33:26 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'news', 'publications', 'awards', 'talks', 'funding']
+const section_names = ['home', 'news', 'publications', 'awards', 'talks', 'funding', 'service']
 
 
 window.addEventListener('DOMContentLoaded', event => {
